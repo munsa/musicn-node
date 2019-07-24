0fb87e3fa63addba3b43bf8060b9aa86e041f214
marc.monserrat <mmonserrat90@gmail.com>
2019-07-24 22:48:50 +0100

    get auth user

--- routes/api/auth.ts
+++ routes/api/auth.ts
@@ -6,6 +6,20 @@ import bcrypt = require('bcryptjs');
 import jwt = require('jsonwebtoken');
 import config = require('config');
 import gravatar = require('gravatar');
+const auth = require('../../middleware/auth');
+
+// @route   POST api/users
+// @desc    Register user
+// @access  Public
+router.get('/', auth, async (req: any, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 // @route   POST api/auth
 // @desc    Authenticate user & get token user
@@ -60,4 +74,5 @@ router.post(
     }
   }
 );
+
 module.exports = router;
