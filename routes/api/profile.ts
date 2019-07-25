25148c7a4662a2a9947a3538bc1ef4148fb7c87f
marc.monserrat <mmonserrat90@gmail.com>
2019-07-25 18:04:33 +0100

    get profile

--- routes/api/profile.ts
+++ routes/api/profile.ts
@@ -1,9 +1,30 @@
 import express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
 
-// @route   GET api/profile
-// @desc    Test route
+// @route   GET api/profile/current
+// @desc    Get current user's profile
 // @access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+router.get('/current', auth, async (req: any, res) => {
+  try {
+    console.log(req.user);
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: 'There is no profile for this user ' });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
