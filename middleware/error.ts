811a3734c87a121250891c7b2000c95b9d1ad4d2
Marc Monserrat Núñez <mmonserrat90@gmail.com>
2020-06-25 17:42:57 +0700

    add comment

--- middleware/error.ts
+++ middleware/error.ts
@@ -1,5 +1,10 @@
 import * as error from '../utils/error';
 
+/***
+ * express app has an error handler middleware that gets 4 parameters (err, req, res, next)
+ * express router middleware does not accept 4 parameters
+ * this function is a wrapper used on each function that wants to handle errors
+ */
 export const handleErrorAsync = func => async (req, res, next) => {
   try {
     await func(req, res, next);
