57d72f761c71084a690e0c1aafe74157cd208b8e
Marc Monserrat Núñez <mmonserrat90@gmail.com>
2020-09-12 09:09:39 +0200

    get credentials

--- services/spotifyService.ts
+++ services/spotifyService.ts
@@ -9,28 +9,32 @@ export module SpotifyService {
   });
 
   // Retrieve an access token.
-  spotifyApi.clientCredentialsGrant().then(
-    function (data) {
-      console.log('The access token expires in ' + data.body['expires_in']);
-      console.log('The access token is ' + data.body['access_token']);
+  const getCredentials = () => {
+    spotifyApi.clientCredentialsGrant().then(
+      function (data) {
+        console.log('The access token expires in ' + data.body['expires_in']);
+        console.log('The access token is ' + data.body['access_token']);
 
-      // Save the access token so that it's used in future calls
-      spotifyApi.setAccessToken(data.body['access_token']);
-    },
-    function (err) {
-      console.log('Something went wrong when retrieving an access token', err);
-    }
-  );
+        // Save the access token so that it's used in future calls
+        spotifyApi.setAccessToken(data.body['access_token']);
+
+        setTimeout(() => {
+          getCredentials();
+        }, data.body['expires_in'] * 1000);
+      },
+      function (err) {
+        console.log('Something went wrong when retrieving an access token', err);
+        getCredentials();
+      }
+    );
+  }
+  getCredentials();
 
   // Get Elvis' albums
   export const getElvis = (): void => {
     spotifyApi.getTrack('4i8ObH2xPP4DSNniUJlgar').then(
       function (data) {
         console.log('Artist albums', data.body);
-        console.log('The access token is ' + spotifyApi.getAccessToken());
-        console.log('The refresh token is ' + spotifyApi.getRefreshToken());
-        spotifyApi.resetAccessToken();
-        spotifyApi.resetRefreshToken();
       },
       function (err) {
         console.error(err);
