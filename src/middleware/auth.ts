fa4ea39e054afc5a777e548e05170418224588c1
Marc Monserrat Núñez <mmonserrat90@gmail.com>
2020-10-06 00:23:01 +0200

    fix env token secret

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,6 @@
 import jwt = require('jsonwebtoken');
-import config = require('config');
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = function(req, res, next) {
   // Get token from header
@@ -12,7 +13,7 @@ module.exports = function(req, res, next) {
 
   // Verify token
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     req.user = decoded.user;
     next();
